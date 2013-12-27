9b4c2244ba6e8ad81bd5d4f365d99504ec3cf7d0
Barak Naveh <baraknaveh@users.noreply.github.com>
2013-12-26 23:21:37 -0800

    added a skeletal vim

--- git/setup-github.sh
+++ git/setup-github.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Sets up github config on a given repository. Must cd to the working copy before running. 
+
 git config user.name "Barak Naveh"
 git config user.email "baraknaveh@users.noreply.github.com"
 git config credential.helper cache
