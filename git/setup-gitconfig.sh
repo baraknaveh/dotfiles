061dbdcd0eb8d1327df10148bcf2e8fed6b9ae4b
Barak Naveh <baraknaveh@users.noreply.github.com>
2013-12-29 02:40:16 -0800

    Made gitconfig more friendly to git prior to v1.8

--- git/setup-gitconfig.sh
+++ git/setup-gitconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Setting ~/.gitconfig to include the gitconfig file in this directory.
-# It's ok to run the script more than once, it will only make changes if  
+# It's ok to run the script more than once, it will only make changes if
 # necessary.
 
 this_dir=$(cd "$(dirname "$0")"; pwd)
@@ -9,15 +9,17 @@ gitconf=~/.gitconfig
 
 if [ ! -f "$gitconf" ]
 then
-	touch "$gitconf"
+        touch "$gitconf"
 fi
 
 my_gitconf="$this_dir/gitconfig"
 
 if ! grep --quiet "$my_gitconf" "$gitconf"
 then
-	echo "Setting $gitconf to include $my_gitconf" 
-	git config --global --add include.path "$my_gitconf"
+        echo "Setting $gitconf to include $my_gitconf"
+        git config --global --add include.path "$my_gitconf"
+        # The following line will only work for git 1.8+
+        git config --global push.default simple
 else
-	echo "$gitconf already includes $my_gitconf"
+        echo "$gitconf already includes $my_gitconf"
 fi
