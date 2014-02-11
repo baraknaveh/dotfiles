ac02869eaa66e504e2450c092c21ddd34e604c99
Barak Naveh <baraknaveh@users.noreply.github.com>
2014-02-11 02:06:17 -0800

    added vim build script for centos

---
+++ vim/build-vim-centos.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Builds Vim from source.
+# The script automates a bunch of steps and (hopefully) ensures that I can
+# have a consistent Vim version across different machines.
+
+echo "Installing required packages..."
+sudo yum groupinstall 'Development Tools'
+sudo yum install ruby perl-devel python-devel ruby-devel perl-ExtUtils-Embed \
+         ncurses-devel
+
+BUILD_DIR="/tmp/vim-build.$USER"
+cd $BUILD_DIR
+
+echo "Building in $BUILD_DIR"
+
+wget -c ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
+tar -xjvf vim-7.4.tar.bz2
+
+cd vim74
+rm -rf src/auto/config.cache
+
+make clean
+./configure --with-features=huge --enable-perlinterp --enable-rubyinterp \
+            --enable-pythoninterp
+make
+
+echo "If the build succeeded, you can install the shiny new vim by typing:"
+echo "  cd $BUILD_DIR"
+echo "  sudo make install"
