0388b5eafa655e8e3119a528ee466a4f2a34a99a
Barak Naveh <baraknaveh@users.noreply.github.com>
2014-02-11 02:39:59 -0800

    minor fixes

--- vim/build-vim-centos.sh
+++ vim/build-vim-centos.sh
@@ -10,8 +10,9 @@ sudo yum install ruby perl-devel python-devel ruby-devel perl-ExtUtils-Embed \
          ncurses-devel
 
 BUILD_DIR="/tmp/vim-build.$USER"
+rm -rf   $BUILD_DIR
 mkdir -p $BUILD_DIR
-cd $BUILD_DIR
+cd       $BUILD_DIR
 
 echo "Building in $BUILD_DIR"
 
@@ -24,9 +25,10 @@ rm -rf src/auto/config.cache
 
 make clean
 ./configure --with-features=huge --enable-perlinterp --enable-rubyinterp \
-            --enable-pythoninterp --with-compiledby="$USER"
+            --enable-pythoninterp
 make
 
+echo
 echo "If the build succeeded, you can install the shiny new vim by typing:"
-echo "  cd $BUILD_DIR"
+echo "  cd $BUILD_DIR/vim74"
 echo "  sudo make install"
