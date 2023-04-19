719e4bb5b10815144cdd1e3f6ea8ad6742cf80ec
Barak Naveh <>
2023-04-19 13:10:00 -0700

    Added Docker env and completion

--- shell/share/x-rc.sh
+++ shell/share/x-rc.sh
@@ -161,3 +161,8 @@ unset LESSCLOSE
 
 # Use sccache to speed up cargo builds
 export RUSTC_WRAPPER="sccache"
+
+# Set up Docker env and completion
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
